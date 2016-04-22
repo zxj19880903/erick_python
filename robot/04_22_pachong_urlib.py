d6976158ede4e7218ec6500ce9563d891dc00a4e
erick <zhaoxinjiang2012@163.com>
2016-04-22 17:15:48 +0800

    测试爬虫爬取页面，每天都要努力，erick

--- robot/04_22_pachong_urlib.py
+++ robot/04_22_pachong_urlib.py
@@ -1,4 +1,41 @@
+'''import urllib2
+
+request = urllib2.Request("https://www.1qianbao.com")
+response = urllib2.urlopen(request)
+print response.read()
+'''
+'''
+import urllib
 import urllib2
 
-response = urllib2.urlopen("http://www.baidu.com")
+values = {"username" : "zhaoxinjiang2012@163.com", "password" : "#######"}
+data = urllib.urlencode(values)
+url = "https://passport.csdn.net/account/login?from=http://my.csdn.net/my/mycsdn"
+request = urllib2.Request(url, data)
+response = urllib2.urlopen(request)
 print response.read()
+'''
+'''
+import urllib
+import  urllib2
+values = {}
+values['username'] = "zhaoxinjiang2012@163.com"
+values['password'] = "######"
+data = urllib.urlencode(values)
+url = "https://passport.csdn.net/account/login?from=http://my.csdn.net/my/mycsdn"
+request = urllib2.Request(url, data)
+response = urllib2 .urlopen(request)
+print response.read()
+'''
+import urllib
+import urllib2
+values = {}
+values['username'] = "zhaoxinjiang2012@163.com"
+values['password'] = "######"
+data = urllib.urlencode(values)
+url = "http://passport.csdn.net/account/login"
+geturl = url + "?" +data
+request = urllib2.Request(geturl)
+response = urllib2.urlopen(request)
+print response.read()
+print  geturl

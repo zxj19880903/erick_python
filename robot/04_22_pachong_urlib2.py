bd226e44aa1a69982207902bf04809af4dae979f
erick <zhaoxinjiang2012@163.com>
2016-04-23 23:49:58 +0800

    学习爬虫继续

---
+++ robot/04_22_pachong_urlib2.py
@@ -0,0 +1,11 @@
+import urllib
+import urllib2
+url = 'http://www.csdn.net'
+user_agent = 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'
+values = {'username' : 'cqc', 'password' : '#####'}
+headers = { 'User-Agent' : user_agent }
+data = urllib.urlencode(values)
+request = urllib2.Request(url, data, headers)
+response = urllib2.urlopen(request)
+page = response.read()
+print page

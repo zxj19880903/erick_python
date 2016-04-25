8766deb7ee768b1d9d29a50e24f64af7d79a0bd9
erick <zhaoxinjiang2012@163.com>
2016-04-25 08:55:33 +0800

    更新防盗链的网站
    防盗链网站—点评

--- robot/04_22_pachong_urlib2.py
+++ robot/04_22_pachong_urlib2.py
@@ -1,9 +1,12 @@
 import urllib
 import urllib2
-url = 'https://www.1qiqnbao.net'
+url = 'http://www.dianping.com/'
 user_agent = 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'
 values = {'username' : 'cqc', 'password' : '#####'}
-headers = { 'User-Agent' : user_agent }
+#headers = { 'User-Agent' : user_agent }
+####dao  lian######
+headers = {'User-Agent': 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)',
+           'Referer': 'http://www.dianping.com/articles'}
 data = urllib.urlencode(values)
 request = urllib2.Request(url, data, headers)
 response = urllib2.urlopen(request)

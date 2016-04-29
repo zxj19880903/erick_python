583f19c959d7a2ffa0228856e882575056890c7d
erick <zhaoxinjiang2012@163.com>
2016-04-29 18:15:20 +0800

    更新0429学习的python爬虫知识

---
+++ robot/04_29_pachong_cookie_login.py
@@ -0,0 +1,23 @@
+# -*- coding:utf-8 -*-
+__author__ = 'erick'
+
+import urllib
+import urllib2
+import cookielib
+
+filename = 'cookie.txt'
+#声明一个MozillaCookieJar对象实例来保存cookie，之后写入文件
+cookie = cookielib.MozillaCookieJar(filename)
+opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cookie))
+postdata = urllib.urlencode({'stuid': 'Jessica2', 'pwd': '802'})
+#登录教务系统的URL
+loginUrl = 'http://139.196.210.106:8080/'
+#模拟登录，并把cookie保存到变量
+result = opener.open(loginUrl, postdata)
+#保存cookie到cookie.txt中
+cookie.save(ignore_discard=True, ignore_expires=True)
+#利用cookie请求访问另一个网址，此网址是成绩查询网址
+gradeUrl = 'http://139.196.210.106:8080/view/main/main.jsp#'
+#请求访问成绩查询网址
+result = opener.open(gradeUrl)
+print result.read()
